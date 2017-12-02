78161c642a06e92a7f57b103c0c7f7a05807596e
haihala <eero.haihala@gmail.com>
2017-12-02 13:42:38 +0200

    added pickup monodevelop

---
+++ Assets/Scripts/Core/Pickup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Pickup : Interactable {
+    public Item Content;
+
+    public override void Interact(Player Plr) {
+        Plr.Gather(Content);
+        Destroy(gameObject);
+    }
+}
