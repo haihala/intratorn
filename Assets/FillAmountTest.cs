642d0788ec9b67dfecdc2522e602eef019ccbdb9
haihala <eero.haihala@gmail.com>
2017-12-04 07:21:22 +0200

    looking at a certain method for health bars

---
+++ Assets/FillAmountTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FillAmountTest : MonoBehaviour {
+    public float Amount;
+    public Image Target;
+    
+	// Update is called once per frame
+	void Update () {
+        Target.fillAmount = Amount;
+	}
+}
