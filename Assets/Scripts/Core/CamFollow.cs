4f0cef589e016f630d7e222532adfa100cb24f81
haihala <eero.haihala@gmail.com>
2017-12-19 12:29:00 +0200

    fine tuned movement and camera following. Player still clips through walls.

---
+++ Assets/Scripts/Core/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+    public Transform target;
+    public float distance;
+
+    float heightOffset;
+    float widthOffset;
+
+    void Start() {
+        heightOffset = distance/(float)Math.Sqrt(2);
+        widthOffset = distance/2;
+    }
+
+	void Update () {
+        transform.position = new Vector3(
+            target.position.x - widthOffset, 
+            target.position.y + heightOffset, 
+            target.position.z - widthOffset
+            );
+    }
+}
