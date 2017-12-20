7ae6765fbe1f1150bad92ea54ecd5d5862ae4700
haihala <eero.haihala@gmail.com>
2017-12-20 06:56:42 +0200

    fixed issue with perspective(used to move faster sideways)

--- Assets/Scripts/Core/PlayerParts/PlayerMover.cs
+++ Assets/Scripts/Core/PlayerParts/PlayerMover.cs
@@ -12,10 +12,9 @@ public class PlayerMover : MonoBehaviour {
 
 
     void FixedUpdate() {
-        var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical") * Mathf.Sqrt(2));
         moveDirection = Camera.main.transform.TransformDirection(moveDirection);
         moveDirection.y = 0;
-
 
         rb.MovePosition(rb.position + moveDirection * Speed.Value * Time.deltaTime);
     }
