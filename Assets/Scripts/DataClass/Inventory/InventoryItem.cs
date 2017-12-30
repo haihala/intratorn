abbbebee981857309fbc15ff9e2e67e4a543ccdc
haihala <eero.haihala@gmail.com>
2017-12-30 18:53:56 +0200

    added type specification to inventory ui (essential, because at the moment all weapons are grouped under weapons)

--- Assets/Scripts/DataClass/Inventory/InventoryItem.cs
+++ Assets/Scripts/DataClass/Inventory/InventoryItem.cs
@@ -4,12 +4,14 @@ using TMPro;
 
 public class InventoryItem : MonoBehaviour {
 	public TextMeshProUGUI Name;
+	public TextMeshProUGUI Type;
 	public TextMeshProUGUI Weight;
 	private InventoryUI master;
 	private Item item;
 
     public void Set_values(Item item, InventoryUI master){
 		Name.text = item.Name;
+		Type.text = item.GetType().ToString();
 		Weight.text = item.Weight.ToString();
 
 		Button btn = GetComponent<Button>();
