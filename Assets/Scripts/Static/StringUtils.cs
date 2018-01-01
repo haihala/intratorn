f86cfe50abde8394addde2d5e89ba86f36afa146
haihala <eero.haihala@gmail.com>
2018-01-01 05:49:54 +0200

    generic string utils class, currently only has substring_until, which returns either the input string or a substring of the inputs string before a given character

---
+++ Assets/Scripts/Static/StringUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class StringUtils {
+	public static string Substring_until(string origin, char target) {
+		int index = origin.IndexOf(target);
+		if (index > 0) {
+			return origin.Substring(0, index);
+		}
+		return origin;
+	}
+}
